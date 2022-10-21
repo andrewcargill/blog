d9fb889a1de9354f66b5c4759ee2f5bec5e630e1
andrewcargill <andycargill01@aol.com>
2022-10-21 09:32:18 +0000

    summernote added and working

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+#this imports the post table into admin
+
+
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+# our blog content (which we know is a text field - we 
+# want to use summernote for this)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_field = ('content')
+
+#before summer note the code was:
+
+# admin.site.register(Post)
+
