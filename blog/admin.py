c692be6c3073e3a79471124a29fd910318a60ef6
andrewcargill <andycargill01@aol.com>
2022-10-21 10:46:51 +0000

    kind of working

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 #this imports the post table into admin
 
 
-from .models import Post
+from .models import Post, Comments
 from django_summernote.admin import SummernoteModelAdmin
 
 # our blog content (which we know is a text field - we 
@@ -11,8 +11,23 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_field = ('content')
+
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+        
+
 
 #before summer note the code was:
 
